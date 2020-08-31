3e80a91c717898c1bf78f9804b5ce2517b8854a5
Joo <63215139+jky0417@users.noreply.github.com>
2020-08-31 17:23:35 +0900

    Fail : Add formatChange

--- js-practice/clock.js
+++ js-practice/clock.js
@@ -1,20 +1,45 @@
 const clockContainer = document.querySelector(".js-clock"),
-    clockTitle = clockContainer.querySelector("h1");
+    clockTitle = clockContainer.querySelector("h1"),
+    btn = document.querySelector(".btn");
+
+    var clear;
+
+function changeFormat(event){
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    const ampm = (hours >= 12) ? "PM" : "AM";
+    hours = (hours == 0) ? 12 : ((hours > 12) ? (hours - 12): hours);
+
+    clockTitle.innerText = `${ampm} ${hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}` : minutes}:${
+            seconds < 10? `0${seconds}` : seconds}`
+    setInterval(changeFormat, 1000);
+}
 
  function getTime(){
-    const date = new Date();
-    const minutes = date.getMinutes();
-    const hours = date.getHours();
-    const seconds = date.getSeconds();
+    // const로 변수 선언시 changeFomat 함수에서 재사용할 수 없기 떄문에 let으로 변경
+    let date = new Date();
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let seconds = date.getSeconds();
 
     clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
         minutes < 10 ? `0${minutes}` : minutes
     }:${seconds < 10 ? `0${seconds}` : seconds}`
+
+    clear = setInterval(getTime, 1000);
+    // setInterval(getTime, 1000);
+    // 버튼 클릭 이벤트 추가
+    // btn.addEventListener("click", changeFormat);
+    
  }
 
 function init() {
     getTime();
-    setInterval(getTime, 1000);
+    btn.addEventListener("click", changeFormat);
 }
 
 init();
