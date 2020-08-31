8af4fabcbaffeebab9d0f7827b8556e5c4f651d7
Joo <63215139+jky0417@users.noreply.github.com>
2020-08-31 14:44:47 +0900

    Add WeatherIcon

--- js-practice/weather.js
+++ js-practice/weather.js
@@ -1,17 +1,23 @@
 const weatherContainer = document.querySelector(".js-weather");
+const weatherIcon = document.querySelector(".weatherIcon");
 
 const API_KEY = "0e18c56712f034c9d4c6429df8431782";
 const COORDS = 'coords';
 
 function getWeather(lat, lng) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric&lang=kr`
     ).then(function(response){
         return response.json();
     })
     .then(function(json){
         const temperature = json.main.temp;
         const place = json.name;
-        weatherContainer.innerText = `${temperature} @ ${place}`
+        const icon = json.weather[0].icon;
+        
+        const iconUrl = "https://openweathermap.org/img/wn/" + icon + ".png";
+
+        $('.weatherIcon').attr('src', iconUrl);
+        weatherContainer.innerText = `${Math.ceil(temperature)} @ ${place}`;
     });
 }
 
